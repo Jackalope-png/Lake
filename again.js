83c556a9aa2ede802df25f7675ac60efc98072b4
Jackalope.png <herofussion107@gmail.com>
2024-06-25 16:43:56 +0800

    pinetour

---
+++ again.js
@@ -0,0 +1,16 @@
+function test(){
+    console.log("hi")
+}
+function increase(x,y){
+    return x +y;
+}
+function decrease(x,y){
+    return x - y;
+}
+function multiply(x,y){
+    return x*y;
+}
+function divide(x,y){
+    return x/y;
+}
+console.log(multiply(7, 4))
